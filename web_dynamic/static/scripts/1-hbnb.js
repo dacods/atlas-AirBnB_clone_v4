74c6034838597de8fc9d690a989ad01980ee2378
dacods <135403583+dacods@users.noreply.github.com>
2024-08-03 15:26:46 -0500

    Created 1-hbnb.py 1-hbnb.html 1-hbnb.js

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    const selectedAmenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if (this.checked) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        const selectedNames = Object.values(selectedAmenities);
+        const selectedIds = Object.keys(selectedAmenities);
+
+        const displayLimit = 3;
+        let displayText = '';
+
+        if (selectedNames.length > displayLimit) {
+            displayText = selectedNames.slice(0, displayLimit).join(', ') + ', ...';
+        } else if (selectedNames.length === 0) {
+            displayText = '&nbsp;';
+        } else {
+            displayText = selectedNames.join(', ');
+        }
+
+        $('.amenities h4').html(displayText);
+    })
+})
